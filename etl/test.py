307b88d5f8785ec9777bc5c89f459ec6400ec703
BManta <novoarodriguezmiguel@gmail.com>
2024-02-21 09:34:30 +0100

    ajout formatage json

--- etl/test.py
+++ etl/test.py
@@ -7,4 +7,4 @@ df = pd.read_csv('../in/titanic.csv')
 df_50 = df.head(50)
 
 # Réécrire les 50 premières lignes dans un nouveau fichier CSV
-df_50.to_json('../out/titanic_50.json', index=False)
+df_50.to_csv('../out/titanic_50.csv', index=False)
--- etl/transform.py
+++ etl/transform.py
@@ -2,6 +2,7 @@
 
 
 import pandas as pd
+import json
 
 def etl_csv_to_json(csv_file, json_file):
     # Extract - Charger les données CSV dans un DataFrame
@@ -10,12 +11,24 @@ def etl_csv_to_json(csv_file, json_file):
     # Transform - Convertir le DataFrame en format JSON
     json_data = df.to_json(orient='records', lines=True)
 
-    # Load - Sauvegarder le JSON dans un fichier
+     # Load - Sauvegarder le JSON dans un fichier
     with open(json_file, 'w') as json_output:
         json_output.write(json_data)
+        
+    data = []
+    with open(json_file, 'r') as f:
+        for line in f:
+            record = json.loads(line)
+            data.append(record)
+            print(json.dumps(record, indent=4))
+
+    with open(json_file, 'w') as f:
+        json.dump(data, f, indent=4)
+
 
 # Exemple d'utilisation
 csv_file_path = '../in/titanic_50.csv'
 json_file_path = '../out/titanic_50.json'
 
 etl_csv_to_json(csv_file_path, json_file_path)
+
